50e3e31f1960de4bd56f40c4a674628421887447
Jovan <mabilinjovan2@gmail.com>
2021-02-12 04:14:01 +0800

    feat: user info

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -14,13 +14,59 @@
       <q-item-section>
         <q-item-label>{{ user.name }}</q-item-label>
       </q-item-section>
+
+      <q-item-section side>
+        <q-btn icon="info" color="primary" flat dense rounded @click="showUserInfo"></q-btn>
+      </q-item-section>
     </q-item>
+    <q-dialog v-model="isDialog">
+      <q-card class="card-dimension q-pa-lg">
+        <div class="grid-row q-mb-md">
+          <div class="grid-column customer-photo">
+            <q-img src="~assets/avatar.svg"></q-img>
+          </div>
+          <div class="grid-column customer-details">
+            <strong>{{ `${getUser.name} (${getUser.username}) ` }}</strong>
+            <div>{{ getUser.phone }}</div>
+            <div>{{ getUser.email }}</div>
+            <div>
+              <a :href="getUser.website" class="text-primary">{{ getUser.website }}</a>
+            </div>
+          </div>
+        </div>
+        <q-card class="q-mb-sm" flat bordered>
+          <q-card-section>
+            <div class="address-details">
+              <div class="label">Address</div>
+              <div class="value">{{ `${getUser.address.street} ${getUser.address.suite} ${getUser.address.city} ${getUser.address.zipcode}` }}</div>
+              <div class="value">
+                <q-chip color="primary" text-color="white" size="sm">{{ `Latitude ${getUser.address.geo.lat}` }}</q-chip>
+                <q-chip color="primary" text-color="white" size="sm">{{ `Longitude ${getUser.address.geo.lng}` }}</q-chip>
+              </div>
+            </div>
+            <div class="address-details">
+              <div class="label">Company</div>
+              <div class="value">{{ getUser.company.name }}</div>
+              <div class="value">"{{ getUser.company.catchPhrase }}"</div>
+              <div class="value">
+                <q-chip v-for="(value, index) in companyBS(getUser.company.bs)" :key="index" color="primary" text-color="white" size="sm">{{ value }}</q-chip>
+              </div>
+            </div>
+          </q-card-section>
+        </q-card>
+      </q-card>
+    </q-dialog>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 export default {
+  data () {
+    return {
+      isDialog: false
+    }
+  },
   computed: {
     ...mapGetters('user', {
       getUserList: 'getUserList',
@@ -28,6 +74,12 @@ export default {
     })
   },
   methods: {
+    companyBS(value) {
+      return value.split(' ')
+    },
+    showUserInfo() {
+      this.isDialog = true;
+    },
     showAlbums(user) {
       this.clearAlbum();
       this.$store.dispatch('user/getUser', user)
@@ -38,3 +90,29 @@ export default {
   },
 }
 </script>
+<style scoped>
+.grid-row {
+  display: block;
+  text-align: center
+}
+.customer-photo {
+  width: 60px;
+  height: 60px;
+  margin: 0 auto;
+}
+.card-dimension {
+  width: 600px;
+}
+
+.address-details {
+  margin-bottom: 10px
+}
+
+.address-details .label {
+  font-weight: 600;
+}
+
+.value-chip {
+  display: inline-block
+}
+</style>
